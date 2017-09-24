db09587dababf99b7f761b7fb3ebf2a2413d9003
1iveowl <liveowl@outlook.com>
2017-09-24 16:46:38 +0200

    wip

--- src/build/build.ps1
+++ src/build/build.ps1
@@ -11,6 +11,7 @@ $msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBui
 &$msbuild ..\interface\IMQTTClient.rx\IMQTTClientRx.csproj /t:Build /p:Configuration="Release"
 &$msbuild ..\main\MQTTClient.rx\MQTTClientRx.csproj /t:Build /p:Configuration="Release"
 &$msbuild ..\main\MQTTClientRx.netstandard20\MQTTClientRx.netstandard20.csproj /t:Build /p:Configuration="Release"
+&$msbuild ..\main\MQTTClientRx.UWP\MQTTClientRx.UWP.csproj /t:Build /p:Configuration="Release"
 
 
 Remove-Item .\NuGet -Force -Recurse
